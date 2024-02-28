09431ccb2d3313228c427de6dd58e551fa6a9c26
qiaolinhan <742954173@qq.com>
2024-02-28 13:38:55 -0500

    adding nvim header

--- lua/config_plugins/try.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# File: try.py
-# Author: Linhan Qiao <qiaolinhan073@gmail.com>
-# Date: 2024-02-28
-# Last Modified By: Linhan Qiao <qiaolinhan073@gmail.com>
-# -----
-# MIT license
-
